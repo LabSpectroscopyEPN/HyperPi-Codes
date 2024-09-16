022c6e8f4ea80e2f1a53c445ea890af0d158b531
LabSpectroscopyEPN <labspec.epn@gmail.com>
2024-09-16 16:29:30 -0500

    Update Pin_Control.py

--- Controlling the motors/Pin_Control.py
+++ Controlling the motors/Pin_Control.py
@@ -9,7 +9,11 @@ ARDUINO_ADDRESS = 0x08
 bus = smbus.SMBus(1)
 
 # Pines usados
-pinlist = [2,3,4,5,6,7,8,9,10,11,12,14,15,16,17]
+pinlist = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17]
+
+# LEDs wavelenghts
+
+# 445, 490, 520, 560, 580, 600, 620, 660, 680, 730, 760, 800, 850, 880, 940, 980
 
 # Función para apagar un pin
 def control_pin(pin, state):
@@ -19,12 +23,15 @@ def control_pin(pin, state):
         print(f"Error: {e}")
 
 # Apagar el pin especificado por el usuario
+# El modulo de reles tiene logica invertida
 def pin_h_l(delay):
     while True:
         try:
             num = int(input('Ingrese el LED que desea encender: '))
-            control_pin(pinlist[num], 0)  # Apagar el pin - enciende LED
+            control_pin(pinlist[num], 0)  # Apagar el pin
             time.sleep(delay)
-            control_pin(pinlist[num], 1)  # Encender el pin - apaga LED
+            control_pin(pinlist[num], 1)  # Encender el pin
         except ValueError:
             print("Por favor, ingresa un número válido.")
+
+pin_h_l(0.5)
